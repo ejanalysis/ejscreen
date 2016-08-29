af12c7ee8da8d288cd800506d34ecc9d6e01c715
ejanalyst <info@ejanalysis.com>
2016-08-29 07:37:37 -0400

    cut near from names.e etc?

--- R/names.evars.R
+++ R/names.evars.R
@@ -5,11 +5,20 @@
 #' @description This data set provides variables that hold the colnames
 #'   of environmental indicator fields in data.frames that may be used in the ejscreen package
 #'   to make it easier to refer to them as a vector, e.g., mydf[ , names.e]
+#' @details NOTE: This used to provide the 2015 version's list, which had "neuro" in it, but now defaults to the latest version. 
 #' @seealso  \code{\link{ejscreenformulas}} \code{\link{names.evars}} \code{\link{names.dvars}} \code{\link{names.ejvars}}
 #' @usage data('names.evars')
 #' @source Names developed for this package. No external data source.
 #' @keywords datasets
-#' @format A series of variables (each is a character vector of colnames):
+#' @format A series of variables (each is a character vector of colnames).
+#' For 2016 version of EJSCREEN:
+#' \itemize{
+#'  \item "names.e" (pm, o3, cancer,     resp, dpm, pctpre1960, traffic.score, proximity.npl, proximity.rmp, proximity.tsdf, proximity.npdes)
+#'  \item "names.e.bin"
+#'  \item "names.e.pctile"
+#'  \item "Elist" (this one is like names.e, but as a list, not a vector)
+#' }
+#' For 2015 version of EJSCREEN it was:
 #' \itemize{
 #'  \item "names.e" (pm, o3, cancer, neuro, resp, dpm, pctpre1960, traffic.score, proximity.npl, proximity.rmp, proximity.tsdf, proximity.npdes)
 #'  \item "names.e.bin"
