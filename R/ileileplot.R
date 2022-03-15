33e2a9a4b93e5e8eb0b2574575a013f9b82e82dc
Mark Corrales <corrales.mark@epa.gov>
2022-03-15 13:15:52 -0400

    Update ileileplot.R

--- R/ileileplot.R
+++ R/ileileplot.R
@@ -1,5 +1,7 @@
 #' Scatter plot of median of Environmental score percentiles for each Demographic score percentile
 #'
+#' Shows median envt percentile of places within each percentile of demog
+#' 
 #' @param x_demog_percentile demographic scores of block groups, as percentiles 0-100 
 #' @param y_envt_percentile environmental indicator scores of block groups, as percentiles 0-100
 #' @param main optional main title for plot
@@ -21,9 +23,7 @@
 #'  pj(1,3)
 #'  
 #'   
-ileile_plot <- function(x_demog_percentile, y_envt_percentile, 
-                        xlab='Demographic percentile (binned by rounding)',
-                        ylab='median of Environmental percentiles',
+ileile_plot <- function(x_demog_percentile, y_envt_percentile, xlab='Demographic percentile (binned by rounding)', ylab='median of Environmental percentiles',
                         main='Does Environmental indicator tend to be higher where Demographic % is higher?', ...) {
   dt <- data.table::data.table(xp=round(x_demog_percentile, 0), yp=y_envt_percentile)
   xyplot <- dt[ , median(yp, na.rm = TRUE), by=xp]
