e6c5b9c2cb3da09d94af25cf328b7b4980487fc8
ejanalyst <info@ejanalysis.com>
2022-09-15 09:52:29 -0400

    rdattr updated to help check and update metadata on vintage of data

--- R/rdattr.R
+++ R/rdattr.R
@@ -1,7 +1,20 @@
-rdattr <- function(package='EJAM', which=c('year', 'ACS', 'released', 'ejscreen_version')) {
-  
+#' helper function in updating the package metadata
+#'
+#' @param package e.g. 'ejscreen'
+#' @param which a vector (not list) of strings, the attributes
+#'
+#' @export
+#'
+rdattr <- function(package='EJAM', which=c('census_version', 'acs_version', 'acs_releasedate', 'ejscreen_version', 'ejscreen_releasedate', 'ejscreen_pkg_data', 'year', 'ACS', 'released')) {
+  # census_version = 2020,
+  # acs_version = '2016-2020',
+  # acs_releasedate = '3/17/2022',
+  # ejscreen_version = '2.1',
+  # ejscreen_releasedate = 'October 2022',
+  # ejscreen_pkg_data = 'bg22'
+
   # The 2017-2021 American Community Survey 5-year estimates are scheduled to be released on Thursday, December 8, 2022.
-  
+
   get1attribute <- function(x, which) try(attr(get(x), which = which))
   # utility to check if year attribute is set on each data file
   if (!isNamespaceLoaded(package)) {stop('package not loaded')}
@@ -19,7 +32,7 @@ rdattr <- function(package='EJAM', which=c('year', 'ACS', 'released', 'ejscreen_
     results <- do.call(cbind, results)
     colnames(results) <- which
   }
- return(results) 
+ return(results)
 }
 
 # rdattr()
--- inst/NOTES_updating_attributes_showing_data_vintage.R
+++ inst/NOTES_updating_attributes_showing_data_vintage.R
@@ -1,74 +1,20 @@
 # setting attributes on datasets that make clear their vintage
 
+
+# see new function   add_metadata()
+
 # note: The 2017-2021 American Community Survey 5-year estimates are scheduled to be released on Thursday, December 8, 2022.
 
-library(EJAM)
-# GO TO THE WORKING DIRECTORY OF THE RIGHT PACKAGE TO DO THESE UPDATES:
+#update metadata for these:
 
+#
+# add_metadata(usastats)
+# add_metadata(regionstats)
+# add_metadata(statestats)
+#
 
-head(t(  usastats[usastats$PCTILE == 'mean', ]))
-head(t(statestats[usastats$PCTILE == 'mean', ]))
-# PUERTO RICO NOT INCLUDED?
+  rdattr()
 
-
-# set attributes to store metadata on vintage
-metadata <- list(
-  census_version = 2020,
-  acs_version = '2016-2020',
-  acs_releasedate = '3/17/2022',
-  ejscreen_version = '2.1',
-  ejscreen_releasedate = 'September 2022',
-  ejscreen_pkg_data = 'bg22'
-)
-  attributes(bg22) <- c(attributes(bg22), metadata)
-
-
-
-
-
-
-
-# older set of attributes:
-
- attr(usastats,    which = 'released') <- 2022
- attr(usastats,    which = 'ejscreen_version') <- '2.0'
- attr(usastats,    which = 'ACS') <- '2015-2019'
- # usethis::use_data(usastats, overwrite = TRUE)
- # √ Saving 'usastats' to 'data/usastats.rda'
- # * Document your data (see 'https://r-pkgs.org/data.html')
- 
- attr(regionstats, which = 'released') <- 2022
- attr(regionstats, which = 'ejscreen_version') <- '2.0'
- attr(regionstats, which = 'ACS') <- '2015-2019'
- # usethis::use_data(regionstats, overwrite = TRUE)
- ### √ Saving 'regionstats' to 'data/regionstats.rda'
- ### * Document your data (see 'https://r-pkgs.org/data.html')
- 
- attr(statestats,  which = 'released') <- 2022
- attr(statestats,  which = 'ejscreen_version') <- '2.0'
- attr(statestats,  which = 'ACS') <- '2015-2019'
- # usethis::use_data(statestats, overwrite = TRUE)
- ### √ Saving 'statestats' to 'data/statestats.rda'
- ### * Document your data (see 'https://r-pkgs.org/data.html')
- 
- 
- rdattr()
-#                      year ACS         released     ejscreen_version
-# blockgroupstats      2021 "2015-2019" "early 2022" "2.0"           
- 
-# usastats             NULL "2015-2019" 2022         "2.0"     
-# regionstats          NULL "2015-2019" 2022         "2.0"           
-# statestats           NULL "2015-2019" 2022         "2.0"           
- 
-# stateinfo            NULL NULL        NULL         NULL            
-# stateregions         NULL NULL        NULL         NULL            
-# statesshp            NULL NULL        NULL         NULL            
- 
-# NAICS                2017 NULL        2022         NULL            
- 
-# points1000example    NULL NULL        NULL         NULL            
-# points100example     NULL NULL        NULL         NULL            
-# sites2blocks_example NULL NULL        NULL         NULL            
 
 
 
